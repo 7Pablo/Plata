0c29bac8634ac5bbf1d7510034f0519d7255c0b8
Pablo Armendariz <juanpaarmendariz3@gmail.com>
2025-06-11 21:51:36 -0500

    Change next config to fit new domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,10 @@
-console.log("Base path:", process.env.NEXT_PUBLIC_BASE_PATH);  
-
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
-  assetPrefix: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
+  basePath: '',
+  assetPrefix: '',
   images: {
     unoptimized: true,
   },
 };
 
 export default nextConfig;
-
